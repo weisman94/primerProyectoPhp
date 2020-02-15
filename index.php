065d0b9ef7cbb7e9dbeed81fb14b7667db72ed1c
501 <501@academicos.cesde.edu.co>
2020-02-14 21:00:51 -0500

    esta es la primer foto funcional del codigo

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+// Esto es un comentario de linea
+/* Este es un comentario
+de varias linea*/
+
+// DEFINIRI VARIABLES
+$nombre     =   "Carlos Navales";
+$edad       =   2;
+$estatura   =   1.76;
+
+//Imprimir variables en el navegador  
+echo($nombre);
+echo($edad);
+
+echo("<br>");
+echo("mi nombre es ".$nombre." y la edad es ".$edad);
+echo("<br>");
+echo("mi nombre es ".$nombre." y la edad es ".$edad);
+
+
+?>
